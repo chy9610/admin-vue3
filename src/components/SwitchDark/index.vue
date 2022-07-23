83f4cf71967abf0a8612957cab7d420d5f02df07
陈化宇 <1171193188@qq.com>
2022-07-23 09:37:18 +0800

    feat：新增公共模板模块

---
+++ src/components/SwitchDark/index.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <el-switch v-model="themeConfig.isDark" @change="switchDark" inline-prompt active-color="#0a0a0a"
+        inactive-color="#dcdfe6" :active-icon="Sunny" :inactive-icon="Moon" />
+</template>
+
+<script lang="ts" setup name="switchDark">
+import { computed } from 'vue';
+import { GlobalStore } from '@/store';
+import { Sunny, Moon } from "@element-plus/icons-vue";
+import { useTheme } from "@/hooks/useTheme";
+
+const globalStore = GlobalStore();
+const { switchDark } = useTheme();
+const themeConfig = computed(() => globalStore.themeConfig)
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
