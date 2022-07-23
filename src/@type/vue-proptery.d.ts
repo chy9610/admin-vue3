980f06df97c91905e2e9ad77615ef83c529dc9c4
陈化宇 <1171193188@qq.com>
2022-07-23 09:34:17 +0800

    feat: 完善类型声明

--- src/@type/vue-proptery.d.ts
+++ src/@type/vue-proptery.d.ts
@@ -1,13 +1,13 @@
 import { ELMessage } from 'element-plus';
 
 declare module '@vue/runtime-core' {
-    interface ComponentCustomProperties {
-        $message: ELMessage
-    }
+	interface ComponentCustomProperties {
+		$message: ELMessage;
+	}
 }
 
 declare module 'vue-router' {
-    interface RouteMeta {
-        roles?: string[]
-    }
+	interface RouteMeta {
+		roles?: string[];
+	}
 }
