0c2da3e1c9c6dd0ab0d1487acaf330db96ea6062
陈化宇 <1171193188@qq.com>
2022-07-23 09:45:36 +0800

    fix: 完善环境配置及新增插件配置，代码检查配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,81 +1,89 @@
-import { ConfigEnv, defineConfig, loadEnv, UserConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJSX from "@vitejs/plugin-vue-jsx";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import ElementPlus from "unplugin-element-plus/vite";
-import { wrapperEnv } from "./src/utils/getEnv";
-
-import path from "path";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { wrapperEnv } from './src/utils/getEnv';
+import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import vueJSX from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import ElementPlus from 'unplugin-element-plus/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
-  const env = loadEnv(mode, process.cwd());
-  const viteEnv = wrapperEnv(env);
-  return {
-    envDir: "./viteEnv", // 加载环境配置指定目录，绝对路径及相对路径都可以
-    envPrefix: ["VITE", "VENUS"], //这个时候，我们可以将VITE_、VENUS_开头的变量统统透传给客户端
-    plugins: [
-      vue(),
-      vueJSX(),
-      ElementPlus({ useSource: true }), // 按需引入组件时，自定义主题
-      AutoImport({ resolvers: [ElementPlusResolver()] }),
-      Components({ resolvers: [ElementPlusResolver()] }),
-    ], // 插件
-    resolve: {
-      // 配置别名
-      alias: [
-        {
-          find: "@",
-          replacement: path.resolve(__dirname, "src"),
-        },
-        {
-          find: "@assets",
-          replacement: path.resolve(__dirname, "src/assets"),
-        },
-        {
-          find: "@views",
-          replacement: path.resolve(__dirname, "src/views"),
-        },
-        {
-          find: "@components",
-          replacement: path.resolve(__dirname, "src/components"),
-        },
-        {
-          find: "@store",
-          replacement: path.resolve(__dirname, "src/store"),
-        },
-        {
-          find: "@utils",
-          replacement: path.resolve(__dirname, "src/utils"),
-        },
-      ],
-    },
-    build: {
-      assetsDir: "assets", //指定静态资源存放路径
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use 'src/assets/css/reset.scss' as *;`, // 引入全局 scss 样式变量文件，文件路径不能使用 @
-        },
-      },
-    },
-    server: {
-      host: "0.0.0.0", // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
-      port: viteEnv.VITE_PORT,
-      open: viteEnv.VITE_OPEN,
-      cors: true,
-      // https: false,
-      // 代理跨域（mock 不需要配置，这里只是个事列）
-      proxy: {
-        "/api": {
-          // target: "https://www.fastmock.site/mock/f81e8333c1a9276214bcdbc170d9e0a0", // fastmock
-          target: "https://mock.mengxuegu.com/mock/629d727e6163854a32e8307e", // easymock
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-  };
+	const env = loadEnv(mode, process.cwd());
+	const viteEnv = wrapperEnv(env);
+	return {
+		envDir: './viteEnv', // 加载环境配置指定目录，绝对路径及相对路径都可以
+		envPrefix: ['VITE', 'VENUS'], //这个时候，我们可以将VITE_、VENUS_开头的变量统统透传给客户端
+		plugins: [
+			vue(),
+			createHtmlPlugin({
+				inject: {
+					data: {
+						title: viteEnv.VITE_GLOB_APP_TITLE,
+					},
+				},
+			}),
+			// * vite 可以使用 jsx/tsx 语法
+			vueJSX(),
+			// * name 可以写在 script 标签上
+			VueSetupExtend(),
+			// 按需引入组件时，自定义主题
+			ElementPlus({ useSource: true }),
+			AutoImport({ resolvers: [ElementPlusResolver()] }),
+			Components({ resolvers: [ElementPlusResolver()] }),
+			// * 是否生成包预览
+			viteEnv.VITE_REPORT && visualizer(),
+			// * gzip compress
+			viteEnv.VITE_BUILD_GZIP &&
+				viteCompression({
+					verbose: true,
+					disable: false,
+					threshold: 10240,
+					algorithm: 'gzip',
+					ext: '.gz',
+				}),
+		], // 插件
+		resolve: {
+			// 文件路径映射
+			alias: {
+				'@': resolve(__dirname, 'src'),
+				'@views': resolve(__dirname, 'src/views'),
+				'@utils': resolve(__dirname, 'src/utils'),
+				'@store': resolve(__dirname, 'src/store'),
+				'@assets': resolve(__dirname, 'src/assets'),
+				'@components': resolve(__dirname, 'src/components'),
+				'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+			},
+		},
+		build: {
+			assetsDir: 'assets', //指定静态资源存放路径
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@use 'src/styles/var.scss' as *;`, // 引入全局 scss 样式变量文件，文件路径不能使用 @
+				},
+			},
+		},
+		server: {
+			host: '0.0.0.0', // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
+			port: viteEnv.VITE_PORT,
+			open: viteEnv.VITE_OPEN,
+			cors: true,
+			// https: false,
+			// 代理跨域（mock 不需要配置，这里只是个事列）
+			proxy: {
+				'/api': {
+					// target: "https://www.fastmock.site/mock/f81e8333c1a9276214bcdbc170d9e0a0", // fastmock
+					target: 'https://mock.mengxuegu.com/mock/629d727e6163854a32e8307e', // easymock
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/api/, ''),
+				},
+			},
+		},
+	};
 });
