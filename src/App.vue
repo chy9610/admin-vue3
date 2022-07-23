5167e6db7f75cade39d0f0120d0f0c62c4163279
陈化宇 <1171193188@qq.com>
2022-07-23 09:46:20 +0800

    fix: 完善入口文件配置

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
-<script setup lang="ts">
-import { ElConfigProvider } from "element-plus";
-import { useAppStore } from '@/store/modules/app/index';
-const appStore = useAppStore()
-const locale = appStore.$state.localeLanguage;
-</script>
 
 <template>
-  <el-config-provider :locale="locale">
+  <el-config-provider :locale="i18Locale">
     <router-view />
   </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { ElConfigProvider } from "element-plus";
+import { GlobalStore } from "@/store";
+
+// 配置element中英文
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import en from "element-plus/es/locale/lang/en";
+
+const globalStore = GlobalStore();
+
+const i18Locale = computed(() => {
+  if (!globalStore.language) {
+    return en
+  }
+  if (globalStore.language === 'zh') return zhCn;
+  if (globalStore.language === 'en') return en;
+})
+
+// element 大小
+// const assemblySize = computed((): string => globalStore.assemblySize)
+// : size ="assemblySize"
+</script>
 
 <style>
 * {
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,41 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
 // 状态管理器
 import { createPinia } from 'pinia';
-const pinia = createPinia()
+import piniaPersist from 'pinia-plugin-persist';
+const pinia = createPinia();
+pinia.use(piniaPersist);
 
-import i18n from '@/locales/index';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-import router from "./router/index";
+import router from './routers/index';
 
-import App from './App.vue'
-createApp(App).use(pinia).use(router).use(i18n).mount('#app')
+import App from './App.vue';
+
+// 引入公共样式文件
+import '@/styles/reset.scss';
+import '@/styles/element.scss';
+import '@/styles/element-dark.scss';
+import '@/styles/common.scss';
+import '@/assets/iconfont/iconfont.scss';
+
+// main.ts是我这个项目的入口文件，相关代码会在客户端执行，在这里打印一下变量
+// vite并非将你写在配置文件中的所有变量（或者说参数）都会透传给客户端，在我们没有特殊配置的时候，它只会识别VITE_开头的参数，我们可以在入口文件中打印一下
+
+/**
+ * import.meta.env.MODE: {string}       应用运行模式
+ * import.meta.env.BASE_URL: {string}   部署应用时的基本 URL，它由 base 配置决定。
+ * import.meta.env.PROD: {boolean}      应用是否运行在生产环境
+ * import.meta.env.DEV: {boolean}       应用是否运行在开发环境
+ */
+
+// 那么我们有没有什么办法能指定读取到哪些参数呢 ？？
+// VITE_ 开头的规则是否能够修改呢 ？？
+// 实际上是可以修改的，我们需要在 vite.config.ts 文件中加入一个参数 envPrefix:
+// envPrefix: ["VITE", "VENUS"], //这个时候，我们可以将VITE_、VENUS_开头的变量统统透传给客户端
+
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+	app.component(key, component);
+}
+app.use(pinia).use(router).mount('#app');
